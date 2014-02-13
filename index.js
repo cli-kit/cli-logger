a7c142bb267e27fd9c954517ed1606edf26661e7
cyberfunk <hello@cyberfunk.org>
2014-02-13 15:06:27 +0800

    Sketch Logger class structure.

--- index.js
+++ index.js
@@ -1,15 +1,91 @@
 var events = require('events');
+var os = require('os');
+var path = require('path'),
+  basename = path.basename;
 var util = require('util');
+var merge = require('cli-util').merge;
 
-var defaults = {
-
+var levels = {
+  trace: 10,
+  debug: 20,
+  info: 30,
+  warn: 40,
+  error: 50,
+  fatal: 60
 }
 
-var Logger = function(conf) {
+var defaults = {
+  name: basename(process.argv[1]),
+  json: true
+}
 
+/**
+ *  Create a Logger instance.
+ *
+ *  @param conf The logger configuration.
+ */
+var Logger = function(conf) {
+  conf = conf || {};
+  this.conf = merge(conf, merge(defaults, {}));
+  this.pid = process.pid;
+  this.hostname = os.hostname();
 }
 
 util.inherits(Logger, events.EventEmitter);
+
+/**
+ *  Retrieve a log record.
+ *
+ *  @api private
+ *
+ *  @param level The log level.
+ *  @param message The log message.
+ *  @param ... The message replacement parameters.
+ */
+Logger.prototype.getLogRecord = function(level, message) {
+  var parameters = [].slice.call(arguments, 2);
+  if(parameters.length) {
+    parameters.unshift(message);
+    message = util.format.apply(util, parameters);
+  }
+  var record = message;
+  if(this.conf.json) {
+    record = {
+      pid: this.pid,
+      hostname: this.hostname,
+      name: this.conf.name,
+      msg: message
+    };
+  }
+  console.log('logging message...%j', record);
+}
+
+/**
+ *  Log a message.
+ *
+ *  @api private
+ *
+ *  @param level The log level.
+ *  @param message The log message.
+ *  @param ... The message replacement parameters.
+ */
+Logger.prototype.log = function(level, message) {
+  if(!message) return false;
+  var record = this.getLogRecord.apply(this, arguments);
+  //console.log('logging message...%s', message);
+}
+
+/**
+ *  Log an info message.
+ *
+ *  @param message The log message.
+ *  @param ... The message replacement parameters.
+ */
+Logger.prototype.info = function() {
+  var args = [].slice.call(arguments, 0);
+  args.unshift(levels.info);
+  this.log.apply(this, args);
+}
 
 module.exports = function(conf) {
   var logger = new Logger(conf);
--- test/unit/logger.js
+++ test/unit/logger.js
@@ -3,9 +3,43 @@ var logger = require('../..');
 var Logger = require('../..').Logger;
 
 describe('cli-logger:', function() {
-  it('should configure logger', function(done) {
+  it('should configure logger (defaults)', function(done) {
     var log = logger();
     expect(log).to.be.instanceof(Logger);
+    done();
+  });
+  it('should configure logger (custom)', function(done) {
+    var name = 'mock-logger';
+    var conf = {name: name};
+    var log = logger(conf);
+    expect(log).to.be.instanceof(Logger);
+    expect(log.conf.name).to.eql(name);
+    done();
+  });
+  it('should handle empty message', function(done) {
+    var log = logger();
+    log.info();
+    done();
+  });
+  it('should log info message', function(done) {
+    var name = 'mock-logger';
+    var conf = {name: name};
+    var log = logger(conf);
+    log.info('a mock info message');
+    done();
+  });
+  it('should log info message (plain)', function(done) {
+    var name = 'mock-logger';
+    var conf = {name: name, json: false};
+    var log = logger(conf);
+    log.info('a mock info message');
+    done();
+  });
+  it('should log info message with parameters', function(done) {
+    var name = 'mock-logger';
+    var conf = {name: name};
+    var log = logger(conf);
+    log.info('a mock %s message', 'info');
     done();
   });
 })
