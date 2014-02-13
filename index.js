edaa926b4db48c31e44343b7ceb40953871a4864
cyberfunk <hello@cyberfunk.org>
2014-02-13 17:00:46 +0800

    Initialize output streams.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var events = require('events');
+var fs = require('fs');
 var os = require('os');
 var path = require('path'),
   basename = path.basename;
 var util = require('util');
+var Writable = require('stream').Writable;
 var merge = require('cli-util').merge;
 
 var levels = {
@@ -26,12 +28,51 @@ var defaults = {
  */
 var Logger = function(conf) {
   conf = conf || {};
-  this.conf = merge(conf, merge(defaults, {}));
+  function filter(t, k, v) {
+    if(k !== 'streams') {
+      t[k] = v;
+    }
+  }
+  this.conf = merge(conf, merge(defaults, {}), filter);
+  this.conf.streams = this.conf.streams || {
+    stream: process.stdout,
+    level: levels.info
+  }
+  this.streams = this.initialize();
   this.pid = process.pid;
   this.hostname = os.hostname();
 }
 
 util.inherits(Logger, events.EventEmitter);
+
+/**
+ *  Initialize the output streams.
+ *
+ *  @api private
+ */
+Logger.prototype.initialize = function() {
+  var streams = [];
+  var conf = this.conf;
+  var source = this.conf.streams;
+  function append(stream, level, name) {
+    streams.push({stream: stream, level: level || levels.info, name: name})
+  }
+  function wrap(source) {
+    append(source.stream, source.level, source.name || conf.name);
+  }
+  if(source && typeof(source) == 'object' && !Array.isArray(source)) {
+    if((source.stream instanceof Writable)
+      || source.stream === process.stdout
+      || source.stream === process.stderr) {
+      wrap(source);
+    }
+  }else if(Array.isArray(source)) {
+    source.forEach(function(source) {
+      wrap(source);
+    })
+  }
+  return streams;
+}
 
 /**
  *  Retrieve a log record.
