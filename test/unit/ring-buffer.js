2a9eb9d5f47fbd1493db34423670bca10c03375b
cyberfunk <hello@cyberfunk.org>
2014-02-17 21:55:33 +0800

    Assert on RingBuffer.flush() closes #15.

--- test/unit/ring-buffer.js
+++ test/unit/ring-buffer.js
@@ -1,7 +1,18 @@
+var fs = require('fs');
+var path = require('path');
 var expect = require('chai').expect;
 var logger = require('../..');
 
 describe('cli-logger:', function() {
+  var method;
+  beforeEach(function(done) {
+    method = process.stderr.write;
+    done();
+  })
+  afterEach(function(done) {
+    process.stderr.write = method;
+    done();
+  })
   it('should create ring buffer', function(done) {
     var name = 'mock-ring-buffer-logger';
     var ringbuffer = new logger.RingBuffer();
@@ -51,5 +62,94 @@ describe('cli-logger:', function() {
       .to.eql(logger.keys.length)
       .to.eql(ringbuffer.limit);
     done();
+  });
+  it('should flush ring buffer records to stderr', function(done) {
+    var name = 'mock-ring-buffer-logger';
+    var ringbuffer = new logger.RingBuffer(
+      {limit: logger.keys.length});
+    process.stderr.write = function(chunk) {
+      expect(chunk).to.eql('mock info message\n');
+      done();
+    }
+    var conf = {
+      name: name,
+      streams: [
+        {
+          stream: ringbuffer,
+          level: logger.TRACE
+        }
+      ]
+    };
+    var log = logger(conf);
+    log.info('mock %s message', 'info');
+    ringbuffer.flush(process.stderr);
+    expect(ringbuffer.records.length).to.eql(0);
+  });
+  it('should flush ring buffer records to stderr (json)', function(done) {
+    var name = 'mock-ring-buffer-logger';
+    var ringbuffer = new logger.RingBuffer(
+      {limit: logger.keys.length, json: true});
+    process.stderr.write = function(chunk) {
+      var record = JSON.parse(chunk);
+      expect(record.msg).to.eql('mock info message');
+      done();
+    }
+    var conf = {
+      name: name,
+      streams: [
+        {
+          stream: ringbuffer,
+          level: logger.TRACE
+        }
+      ]
+    };
+    var log = logger(conf);
+    log.info('mock %s message', 'info');
+    ringbuffer.flush(process.stderr);
+    expect(ringbuffer.records.length).to.eql(0);
+  });
+  it('should flush ring buffer records to file (json)', function(done) {
+    var name = 'mock-ring-buffer-logger';
+    var ringbuffer = new logger.RingBuffer(
+      {limit: logger.keys.length, json: true});
+    var conf = {
+      name: name,
+      streams: [
+        {
+          stream: ringbuffer,
+          level: logger.TRACE
+        }
+      ]
+    };
+    var log = logger(conf);
+    log.info('mock %s message', 'info');
+    ringbuffer.flush(path.join('log', name + '.log'));
+    expect(ringbuffer.records.length).to.eql(0);
+    done();
+  });
+  it('should emit error event on closed stream (json)', function(done) {
+    var name = 'mock-ring-buffer-logger';
+    var ringbuffer = new logger.RingBuffer(
+      {limit: logger.keys.length, json: true});
+    var conf = {
+      name: name,
+      streams: [
+        {
+          stream: ringbuffer,
+          level: logger.TRACE
+        }
+      ]
+    };
+    ringbuffer.on('error', function(e) {
+      expect(e).to.be.instanceof(Error);
+      done();
+    })
+    var log = logger(conf);
+    log.info('mock %s message', 'info');
+    var stream = fs.createWriteStream(
+      path.join('log', name + '.log'), {flags: 'w'});
+    stream.end();
+    ringbuffer.flush(stream);
+    expect(ringbuffer.records.length).to.eql(0);
   });
 })
